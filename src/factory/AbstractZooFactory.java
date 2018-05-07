6b2e37cbb5f1157ac1bfad6a5c0c45fb31199853
ha1m <38317929+ha1m@users.noreply.github.com>
2018-05-07 17:41:41 +0300

    Update AbstractZooFactory.java

--- src/factory/AbstractZooFactory.java
+++ src/factory/AbstractZooFactory.java
@@ -3,9 +3,7 @@ package factory;
 import animals.Animal;
 /**
  * 
- * @author Haim Nahmani & Kfir Mazliah
- * ID: 203141197 , 305371866 
- * Campus: Beer Sheva
+ * @author Haim Nahmani 
  */
 public interface AbstractZooFactory {
 	public Animal produceAnimal(String animal, int sz, int hor, int ver, String c);
