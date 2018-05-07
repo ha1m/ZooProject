30b951ba67556993c26e8f2868fd62eb4aa1a05b
ha1m <38317929+ha1m@users.noreply.github.com>
2018-05-07 17:54:55 +0300

    Update IDrawable.java

--- src/graphics/IDrawable.java
+++ src/graphics/IDrawable.java
@@ -3,9 +3,7 @@ package graphics;
 import java.awt.Graphics;
 /**
  * class represent drawing
- * @author Haim Nahmani & Kfir Mazliah
- * ID: 203141197 , 305371866 
- * Campus: Beer Sheva
+ * @author Haim Nahmani 
  */
 public interface IDrawable {
 	 public final static String PICTURE_PATH = "C:\\Users\\Haim Nahmani\\Desktop\\pictures\\pictures\\";
